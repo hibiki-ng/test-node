b6c016a9da3bb05782d8b2a8ada70a3cf775eb46
unknown <zerdiix.zanishiku@gmail.com>
2023-03-01 17:09:00 +0100

    first switch to memo

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
 var indexRouter = require('./routes/index');
 var quotesRouter = require('./routes/quotes');
 
 var app = express();
 
+const uri = "mongodb+srv://hibiki:test123@cluster0.akkrkpr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected to MongoDB Atlas cluster");
+  }
+});
+
 app.use(logger('dev'));
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
 var router = express.Router();
 
+const uri = "mongodb+srv://hibiki:test123@cluster0.akkrkpr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({message: 'alive'});
+router.get('/', (req, res) => {
+  res.send('Homepage');
+});
+
+router.post('/insert', (req, res) => {
+  const data = req.body;
+  console.log(data);
+  const value = data.value;
+  const user = data.user;
+
+  if (!user) {
+    return res.status(400).send("Missing user parameter in request body");
+  }
+
+  const collection = client.db("test").collection(user);
+  collection.insertOne({ value })
+        .then(result => {
+            console.log("Value inserted into database");
+            console.log(res.headersSent); // Outputs: false
+            res.send("Value inserted into database: " + value);
+            console.log(res.headersSent); // Outputs: true
+        })
+        .catch(err => {
+            console.log(err);
+            return res.status(500).send("Error inserting data into database");
+        });
+});
+
+router.post('/getData', (req, res) => {
+  const user = req.body.user;
+  const collection = client.db("test").collection(user);
+
+  collection.find({}).toArray()
+    .then(docs => {
+      console.log("Data retrieved from database:", docs);
+      res.send(docs);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).send("Error retrieving data from database");
+    });
 });
 
 module.exports = router;
